d7ff4ce6cd016c103239cdf614e1989aac877114
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-07-19 09:01:35 +0200

    changed msg to PoseWithCovarianceStamped false msg PoseStamped not working in noetic

--- scripts/pub_wall_pose.py
+++ scripts/pub_wall_pose.py
@@ -2,18 +2,18 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import String
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped
 
 def talker():
-    pub = rospy.Publisher('wall_frame_base',  PoseStamped, queue_size=10)
+    pub = rospy.Publisher('wall_frame_base',  PoseWithCovarianceStamped, queue_size=10)
     rospy.init_node('pub_wall_pose', anonymous=True)
     
-    pose_wall = PoseStamped()
-    pose_wall.pose.position.x = 4.8
-    pose_wall.pose.position.y = 10.0
+    pose_wall = PoseWithCovarianceStamped()
+    pose_wall.pose.pose.position.x = 4.8
+    pose_wall.pose.pose.position.y = 10.0
     
-    pose_wall.pose.orientation.z = 0.687
-    pose_wall.pose.orientation.w = 0.725
+    pose_wall.pose.pose.orientation.z = 0.687
+    pose_wall.pose.pose.orientation.w = 0.725
     pub.publish(pose_wall)
 
 if __name__ == '__main__':
