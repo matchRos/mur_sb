107c20769d26dd77f7153cde49d8f69763ed345a
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-09-08 23:16:43 +0200

    added modified files from other repositories other repos shouldnt override changes made by Alex

---
+++ control/bauschaum/mypath.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+
+
+
+class Mypath:
+    x = None
+    y = None
+    z = None
+    phi = None 
+    v = None
+    w = None
+
+
+
+class MyTrajectory:
+    x = None
+    y = None
+    phi = None 
+    v = None
+    w = None
