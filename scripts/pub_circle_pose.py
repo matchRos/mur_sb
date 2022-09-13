ad20195ffa35e0e55bd47fc528adc26be045f00a
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-09-13 12:11:29 +0200

    added circle as printable object

---
+++ scripts/pub_circle_pose.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+def talker():
+    pub = rospy.Publisher('wall_frame_base',  PoseWithCovarianceStamped, queue_size=10)
+    rospy.init_node('pub_wall_pose', anonymous=True)
+    
+    pose_wall = PoseWithCovarianceStamped()
+    pose_wall.pose.pose.position.x = 0.0
+    pose_wall.pose.pose.position.y = 0.0
+    
+    pose_wall.pose.pose.orientation.z = 0.0
+    pose_wall.pose.pose.orientation.w = 1.0
+    pub.publish(pose_wall)
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
