8a74e3c2873d8d844b912e440418828deac1b08e
matchRos <Recker@match.uni-hannover.de>
2022-09-19 17:38:02 +0200

    removed a lot of unused code

--- control/ur_start_pose.py
+++ control/ur_start_pose.py
@@ -80,7 +80,6 @@ class robot():
                 return  # remaining code of function doesnt need to be called
             request = rospy.get_param('/mir_initialized', False)
             rospy.loginfo("Wait for MiR to be initialized...")
-            # rospy.loginfo(f"MiR initialized: {str(request)}")
             rate.sleep()
         
         
@@ -90,7 +89,7 @@ class robot():
         # Goal-transformation from UR-Base to TCP
         t = trans()
         tcp_t_ur_euler, tcp_t_ur_quat = t.compute_transformation(path, mir_pos)
-        rospy.loginfo(f"tcp_t_ur_quat: {tcp_t_ur_quat}")
+
 
         # for MoveIt: base_footprint is reference link
         tMat_tcp = transformations.euler_matrix(tcp_t_ur_euler[3], tcp_t_ur_euler[4], tcp_t_ur_euler[5], 'sxyz')
@@ -98,15 +97,15 @@ class robot():
         tMat_tcp[1][3] = tcp_t_ur_quat[1]
         tMat_tcp[2][3] = tcp_t_ur_quat[2]
         tMat_tcp=np.array(tMat_tcp)
-        rospy.loginfo(f"tMat_tcp: {tMat_tcp}")
+
         self.listener.waitForTransform("/mur216/UR16/base_link", "/mur216/base_footprint", rospy.Time(), rospy.Duration(4.0))
         transf=self.listener.lookupTransform('/mur216/base_footprint','/mur216/UR16/base_link', rospy.Time(0))
-        rospy.loginfo(f"looking up transform to base_footprint for moveit in sim. transf= {transf}")
+
         baseTfootprint = np.diag([1.0,1.0,1.0,1.0])
         r_baseTfootprint = transf[0]    # keine Drehung
-        rospy.loginfo(f"r_baseTfootprint: {r_baseTfootprint}")
+
         baseTfootprint[0:3,3] = r_baseTfootprint
-        rospy.loginfo(f"baseTfootprint: {baseTfootprint}")
+
         # tMat_ur_tcp = inverseTransformationMat(t.mirbase_T_urbase) @ tMat_tcp
         tMat_ur_tcp = baseTfootprint @ tMat_tcp
         rospy.loginfo(f"tMat_ur_tcp: {tMat_ur_tcp}")
@@ -128,10 +127,10 @@ class robot():
 
             # test for switched goal: TODO: check if this is necessary
             ik.goal_pose = [*self.first_pose_rel.position.__reduce__()[2], *self.first_pose_rel.orientation.__reduce__()[2]]
-            rospy.loginfo(f"goal_pose: {ik.goal_pose}")
+
             ik.solutions = []
             ik.calc_solutions([],0)
-            rospy.loginfo("Moegliche Loesungen first_pose_rel")
+
             for sol in ik.solutions:
                 #test_solution(sol)
                 rospy.loginfo(sol)
@@ -147,78 +146,7 @@ class robot():
 
             self.acceleration = 0.0
             self.i = 0.0       #TODO: wofuer?
-         
-    # def joint_vel(self, i, joint_goal, velocity=0.05):
-    #     """Return positive/negative velocity depending on joint goal direction
-
-    #     Args:
-    #         i (integer): idx of joint
-    #         joint_goal (list[float]): joint goal to reach
-    #         velocity (float, optional): velocity to return. Defaults to 0.05.
-
-    #     Returns:
-    #         float: positive/negative velocity or 0 if goal reached
-    #     """
-    #     curr_joints = self.joint_states #[0.0, 0.0, 0.0, 0.0, 0.0, 0.0] #self.group.get_current_joint_values()
-    #     if curr_joints[i] < joint_goal[i] - 0.001:
-    #         return velocity
-    #     elif curr_joints[i] > joint_goal[i] + 0.001:
-    #         return (-1) * velocity
-    #     else:            
-    #         return 0.0
-        
-    # def calc_accel(self, distance_to_goal):
-    #     """Calculate Acceleration to accel slowly and decel before reaching goal
-
-    #     Args:
-    #         distance_to_goal (float): distance to goal (norm2 of goal minus eef_pos)
-
-    #     Returns:
-    #         float: Acceleration factor between 0 and 1
-    #     """
-    #     # Accelerate slowly
-    #     if self.acceleration < 61:
-    #         accel = self.acceleration/60
-    #         self.acceleration = self.acceleration + 1
-    #     else:
-    #         accel = 1
-
-    #      # Slow down if near goal pos
-    #     if distance_to_goal < 0.08:
-    #         decel = distance_to_goal/0.08
-    #         if decel < 0.1:
-    #             decel = 0.1       
-    #     else:
-    #         decel = 1
-
-    #     return accel*decel
-         
-    # def run(self):
-    #     """Publishes Joint velocities to reach EEF-goal. So that all joints approx. reach goal at same time.
-    #     """
-    #     # rospy.loginfo(self.i)
-    #     self.i += 1.0
-
-    #     distance_to_goal = math.sqrt(sum((self.goal[i] - self.current_ee_pos[i])**2 for i in range(3)))
-    #     if not self.i%50:   # dont spam
-    #         rospy.loginfo(f"distance_to_goal: {distance_to_goal}")
-    #     accel = self.calc_accel(distance_to_goal)
-
-    #     # get max Joint Diff
-    #     max_joint_diff = max(map(abs, self.joint_togoal_diff))
-    #     # rospy.loginfo(f"max_joint_diff: {max_joint_diff}")
-
-    #     cmd_joint_vel = [accel * self.joint_vel(i, self.joint_goal) * abs(self.joint_togoal_diff[i]) / abs(max_joint_diff) for i in range(6)]
-    #     self.joint_group_vel.data = cmd_joint_vel 
-               
-    #     if all(v<0.1 for v in cmd_joint_vel) and distance_to_goal < 0.05:
-    #         rospy.set_param("/ur_initialized", True) 
-    #         self.joint_group_vel.data = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    #         self.joint_vel_pub.publish(self.joint_group_vel)   # TODO: warum nicht? bzw. if am Schluss? --> Restbewegung?
-    #         rospy.signal_shutdown("Position reached...")
-        
-    #     self.joint_vel_pub.publish(self.joint_group_vel)
-        
+                
         
     def joint_states_cb(self, data):  
         a = data #a = joint_states_mixed
@@ -269,22 +197,13 @@ class trans():
             trans_euler, trans_quaternion
             list[float]: [x,y,z, alpha,beta,gamma], list[float]: [x,y,z, a,b,c,d]
         """
-        #Tranformation World - MiR-Base
-        rospy.loginfo("Zielpose")
-        rospy.loginfo(path.poses[0].pose)
-        #rospy.loginfo("MiR Pose")
-        #rospy.loginfo(mir_pos)
-        
+
         mir_trans = [mir_pos.position.x, mir_pos.position.y, mir_pos.position.z]
         mir_rot = [mir_pos.orientation.x, mir_pos.orientation.y, mir_pos.orientation.z, mir_pos.orientation.w]
         world_T_mirbase = transformations.quaternion_matrix(mir_rot)
         world_T_mirbase[0][3] = mir_trans[0]
         world_T_mirbase[1][3] = mir_trans[1]
         world_T_mirbase[2][3] = mir_trans[2]
-        
-        #rospy.loginfo("")
-        #rospy.loginfo(mirbase_T_urbase)
-        #rospy.loginfo("")
         
         world_t_ur = np.dot(world_T_mirbase, self.mirbase_T_urbase)
         # ur_t_world = np.linalg.inv(world_t_ur)
@@ -299,10 +218,6 @@ class trans():
         x1_world = np.matrix([[first_pose.position.x], [first_pose.position.y], [first_pose.position.z], [1]])
         #rospy.loginfo(x1_world)
         x1_urbase = ur_t_world @ x1_world
-        rospy.loginfo(f"Z World: {x1_world[2]}")
-        rospy.loginfo(f"Z Base: {x1_urbase[2]}")
-        #rospy.loginfo("x to urbase")
-        #rospy.loginfo(x1_urbase)
         tcp_t_ur_euler = [x1_urbase.item(0), x1_urbase.item(1), x1_urbase.item(2) + 0.3, 0, math.pi, -urbase_euler[2]]
         quat = transformations.quaternion_from_euler(tcp_t_ur_euler[3], tcp_t_ur_euler[4], tcp_t_ur_euler[5])
         tcp_t_ur_quat = [tcp_t_ur_euler[0], tcp_t_ur_euler[1], tcp_t_ur_euler[2], quat[0], quat[1], quat[2], quat[3]]
@@ -320,6 +235,8 @@ if __name__ == "__main__":
     ur.group.go(ur.joint_goal, wait=True)
 
     ur.group.stop() 
+    switch_to_velocity()
+    rospy.set_param("/ur_initialized", True)
     # rate = rospy.Rate(100)
     # if not use_moveit:
     #     switch_to_velocity()
